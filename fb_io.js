cb96b433aaa2e5ec70fc0ce97728136e8e9b3ce5
alexis-022 <me@alexishood.net>
2026-06-06 11:40:58 +1200

    6/6/26 - Firebase simple write

--- fb_io.js
+++ fb_io.js
@@ -1,4 +1,7 @@
-//firebase.database().ref('/game1/uid2/userName2').set('GAME OVER');
-//fb_writeHighScore(){
-  //  console.log('blah')
-//}
+function fb_writeHighScore(score) {
+  console.log("fb_writeHighScore - fb_io")
+  console.log(score)
+  firebase.database().ref('/game1/uid2/userName2').set(score);
+  //uid
+  //username
+}
